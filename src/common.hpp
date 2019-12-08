8213ed20ccf75d241c84b63a430d228b5b19641a
Gianluca Recchia <gianluca.recchia97@gmail.com>
2019-12-08 10:14:36 +0100

    Add utility function to time code snippets

--- src/common.hpp
+++ src/common.hpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -16,5 +17,32 @@ std::ifstream bootstrap(int, char const**);
 std::string load_file(std::ifstream in);
 
 std::vector<std::string_view> split(std::string_view, char const*);
+
+template<class Unit, class Function>
+void time_execution(Function const& f) noexcept(noexcept(f))
+{
+	using namespace std::chrono;
+
+	char const* unit;
+
+	if constexpr (std::is_same_v<Unit, nanoseconds>)
+		unit = "ms";
+	else if (std::is_same_v<Unit, microseconds>)
+		unit = "\u00b5s";
+	else if (std::is_same_v<Unit, milliseconds>)
+		unit = "ms";
+	else if (std::is_same_v<Unit, seconds>)
+		unit = "s";
+	else if (std::is_same_v<Unit, minutes>)
+		unit = "mins";
+	else if (std::is_same_v<Unit, hours>)
+		unit = "hrs";
+
+	auto const start = steady_clock::now();
+	f();
+	auto const end = steady_clock::now();
+
+	std::cout << "Elapsed: " << duration_cast<Unit>(end - start).count() << unit << '\n';
+}
 
 #endif // SRC_COMMON_HPP
